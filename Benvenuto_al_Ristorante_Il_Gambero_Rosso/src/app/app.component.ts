9a97d8a7819bb8a1711539a082d458c056b63127
AbdullahAlHraishawi <abdullah.alhraishawi@gmail.com>
2022-10-15 10:51:14 +0200

    Third CodeReview

---
+++ Benvenuto_al_Ristorante_Il_Gambero_Rosso/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Benvenuto_al_Ristorante_Il_Gambero_Rosso';
+}
